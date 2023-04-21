3f3af374970284dfd1d668bd05d75387e066c8a4
Aeciopereira <aeciojunior9978087732016@gmail.com>
2023-04-21 19:03:27 -0300

    pop-up  em JS
    Java Script

---
+++ index.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button")
+const modal =  document.querySelector("dialog")
+const buttonclose =document.querySelector("dialog button")
+
+button.onclick = function () {
+     modal.showModal();
+
+}
+
+buttonclose.onclick = function () {
+    modal.close ()
+}
